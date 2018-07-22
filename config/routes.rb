1886997b356d6c42fb7fb46ef3f3b2d3720c32d5
timika <timika.hopkins@gmail.com>
2018-07-22 13:08:28 -0400

    added show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :client do 
 
     get "/students" => "students#index"
-
+    get "/students" => "students#show"
 
   end 
   # STEP 1: A ROUTE triggers a controller action
